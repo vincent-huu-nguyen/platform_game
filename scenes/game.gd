940a285bf478a5e8043ba42d70cd89d8e229e1a4
vinsanite <vincentnguyen7011@gmail.com>
2024-07-28 00:01:41 -0500

    added reset button (enter) bc a bug sometimes happen that doesnt reset the game when a player dies

--- scenes/game.gd
+++ scenes/game.gd
@@ -1,5 +1,10 @@
 extends Node2D
 
+func _physics_process(delta):
+	if Input.is_action_pressed("reset"):
+		Engine.time_scale = 1.0
+		get_tree().reload_current_scene()
+
 func _on_area_2d_body_entered(body):
 	if body is Player:
 		print("win")
