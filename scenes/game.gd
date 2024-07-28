6cbece4438822a4f38f2a306975e749b1b347b2c
vinsanite <vincentnguyen7011@gmail.com>
2024-07-28 05:57:58 -0500

    Worked on main menu

--- scenes/game.gd
+++ scenes/game.gd
@@ -1,5 +1,13 @@
 extends Node2D
 
+@onready var stuckTimer = $if_stuck_timer
+
+func _process(delta): # doesnt work idk why
+	if Engine.time_scale == 0.5:
+		if not stuckTimer.is_stopped():
+			stuckTimer.start()
+			print("Timer started")
+
 func _physics_process(delta):
 	if Input.is_action_pressed("reset"):
 		Engine.time_scale = 1.0
@@ -8,3 +16,7 @@ func _physics_process(delta):
 func _on_area_2d_body_entered(body):
 	if body is Player:
 		print("win")
+
+func _on_if_stuck_timer_timeout(): # doesnt work idk why
+	print("Timer timeout reached, reloading scene")
+	get_tree().reload_current_scene()
--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -2,11 +2,14 @@ extends Control
 
 var stage_one = "res://scenes/game.tscn"
 
+@onready var mainmenu = $MainMenu
 @onready var credits = $Credits
+@onready var modes = $ModeSelect
+@onready var ai_instructions = $ai_HowToPlay
 
-func _on_start_pressed():
-	get_tree().change_scene_to_file(stage_one)
-
+func _on_play_pressed():
+	mainmenu.hide()
+	modes.show()
 
 func _on_settings_pressed():
 	pass # Replace with function body.
@@ -22,3 +25,17 @@ func _on_quit_pressed():
 
 func _on_close_credits_pressed():
 	credits.hide()
+
+
+func _on_ai_pressed():
+	modes.hide()
+	ai_instructions.show()
+
+
+func _on_back_pressed():
+	modes.hide()
+	mainmenu.show()
+
+
+func _on_start_pressed():
+	get_tree().change_scene_to_file(stage_one)
