d81238cf19de1e176c21c534aa647c760d7ad8bc
vinsanite <vincentnguyen7011@gmail.com>
2024-07-31 22:06:41 -0500

    REAL COMPLETE VERSION 1 (Highscore saved)

--- scenes/ai_player.gd
+++ scenes/ai_player.gd
@@ -50,7 +50,7 @@ func _ready():
 	player = get_tree().root.get_node("Game/Player") # Adjust path to player node
 
 	if Global.score >= 20:
-		rate_of_fire = max(0.1 - (0.001 * (Global.score-19)), 0.01)
+		rate_of_fire = max(0.1 - (0.001 * (Global.score-19)), 0.01) #rate of fire increases by score, lowest is 0.01
 
 # Called every frame
 func _process(delta):
--- scripts/global.gd
+++ scripts/global.gd
@@ -1,11 +1,15 @@
 extends Node
 
-# Score variable
+const SAVE_FILE_PATH = "user://savedata.save"
+
+# Score variables
 var score = 0
 var highscore = 0
 var permanant_buff = false
 
 func _ready():
+	# Load the highscore when the game starts
+	load_highscore()
 	# Initial check to update highscore if score is loaded from a saved game
 	check_highscore()
 
@@ -14,8 +18,18 @@ func reset_score():
 	score = 0
 
 func check_highscore():
-	if score > highscore:
+	if score > highscore and !permanant_buff:
 		highscore = score
+		save_highscore()
+
+func save_highscore():
+	var file = FileAccess.open(SAVE_FILE_PATH, FileAccess.WRITE_READ)
+	file.store_32(highscore)
+
+func load_highscore():
+	var file = FileAccess.open(SAVE_FILE_PATH, FileAccess.READ)
+	if FileAccess.file_exists(SAVE_FILE_PATH):
+		highscore = file.get_32()
 
 func perma_buff():
 	permanant_buff = true
